e24f4caaa27ba0f34ead9720b48ac6327cdc39e1
bakial <manhtuan88394@gmail.com>
2021-02-03 22:55:52 -0500

    Add function that check which button is pressed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const buttonColours = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
+let userClickedPattern = [];
 
 //Function that create a new pattern of the game
 function nextSequence() {
@@ -10,4 +11,34 @@ function nextSequence() {
     let randomChosenColour = buttonColours[randomNumber];
     //Push that random colour into the pattern
     gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+    //playSound(randomChosenColour);
 }
+
+//Play the sound of the button
+function playSound(key) {
+    switch (key) {
+        case "red":
+            break;
+    
+        case "green":
+            break;
+            
+        case "blue":
+            break;
+        
+        case "yellow":
+            break;
+        
+        default:
+            break;
+    }
+}
+
+//Record which button is clicked by the user
+$(".btn").on("click", function(event) {
+    let userChosenColour = event.currentTarget.id;
+    userClickedPattern.push(userChosenColour);
+    console.log(userClickedPattern);
+})
