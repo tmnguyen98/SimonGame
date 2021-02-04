7f946489f9f6de794a85595c2a9af700050ae14a
bakial <manhtuan88394@gmail.com>
2021-02-03 21:13:22 -0500

    Create a new Pattern

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-alert("Hello");
+//Array hold seuqence of colours
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+//Function that create a new pattern of the game
+function nextSequence() {
+    let randomNumber = ~~(Math.random()*4);
+    //Choose a random colour
+    let randomChosenColour = buttonColours[randomNumber];
+    //Push that random colour into the pattern
+    gamePattern.push(randomChosenColour);
+}
